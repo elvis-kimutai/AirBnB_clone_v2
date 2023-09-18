00948340eaae866e418567c7f4c081d1e8a284a7
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-09-18 21:59:19 +0000

    MySQL setup test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- the script that prepares a MySQL server for the project
+-- create a database, a new user and grant privileges
+-- set password to hbnb_test_pwd
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
